369b974c25ffd5a72592328582e42ef018515dac
guspriaman <guswarasi@gmail.com>
2022-08-13 10:11:07 +0700

    first commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,295 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>AdminLTE 3 | Dashboard 2</title>
+
+<link rel="stylesheet" href="<?php echo base_url('assets')?>/https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+<link rel="stylesheet" href="<?php echo base_url('assets')?>/plugins/fontawesome-free/css/all.min.css">
+
+<link rel="stylesheet" href="<?php echo base_url('assets')?>/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+
+<link rel="stylesheet" href="<?php echo base_url('assets')?>/dist/css/adminlte.min.css?v=3.2.0">
+
+
+<script nonce="fe38c61b-dac6-466f-a0b8-bdb85557816c">(function(w,d){!function(a,e,t,r){a.zarazData=a.zarazData||{};a.zarazData.executed=[];a.zaraz={deferred:[]};a.zaraz.q=[];a.zaraz._f=function(e){return function(){var t=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:t})}};for(const e of["track","set","ecommerce","debug"])a.zaraz[e]=a.zaraz._f(e);a.zaraz.init=()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r),n=e.getElementsByTagName("title")[0];n&&(a.zarazData.t=e.getElementsByTagName("title")[0].text);a.zarazData.x=Math.random();a.zarazData.w=a.screen.width;a.zarazData.h=a.screen.height;a.zarazData.j=a.innerHeight;a.zarazData.e=a.innerWidth;a.zarazData.l=a.location.href;a.zarazData.r=e.referrer;a.zarazData.k=a.screen.colorDepth;a.zarazData.n=e.characterSet;a.zarazData.o=(new Date).getTimezoneOffset();a.zarazData.q=[];for(;a.zaraz.q.length;){const e=a.zaraz.q.shift();a.zarazData.q.push(e)}z.defer=!0;for(const e of[localStorage,sessionStorage])Object.keys(e||{}).filter((a=>a.startsWith("_zaraz_"))).forEach((t=>{try{a.zarazData["z_"+t.slice(7)]=JSON.parse(e.getItem(t))}catch{a.zarazData["z_"+t.slice(7)]=e.getItem(t)}}));z.referrerPolicy="origin";z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData)));t.parentNode.insertBefore(z,t)};["complete","interactive"].includes(e.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script></head>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+
+<div class="preloader flex-column justify-content-center align-items-center">
+<img class="animation__wobble" src="assets/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+</div>
+
+<nav class="main-header navbar navbar-expand navbar-dark">
+
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+</li>
+<li class="nav-item d-none d-sm-inline-block">
+<a href="index3.html" class="nav-link">Home</a>
+</li>
+<li class="nav-item d-none d-sm-inline-block">
+<a href="#" class="nav-link">Contact</a>
+</li>
+</ul>
+
+<ul class="navbar-nav ml-auto">
+
+<li class="nav-item">
+<a class="nav-link" data-widget="navbar-search" href="#" role="button">
+<i class="fas fa-search"></i>
+</a>
+<div class="navbar-search-block">
+<form class="form-inline">
+<div class="input-group input-group-sm">
+<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+<div class="input-group-append">
+<button class="btn btn-navbar" type="submit">
+<i class="fas fa-search"></i>
+</button>
+<button class="btn btn-navbar" type="button" data-widget="navbar-search">
+<i class="fas fa-times"></i>
+</button>
+</div>
+</div>
+</form>
+</div>
+</li>
+
+<li class="nav-item dropdown">
+<a class="nav-link" data-toggle="dropdown" href="#">
+<i class="far fa-comments"></i>
+<span class="badge badge-danger navbar-badge">3</span>
+</a>
+<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+<a href="#" class="dropdown-item">
+
+<div class="media">
+<img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+<div class="media-body">
+<h3 class="dropdown-item-title">
+Brad Diesel
+<span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+</h3>
+<p class="text-sm">Call me whenever you can...</p>
+<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+</div>
+</div>
+
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+
+<div class="media">
+<img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+<div class="media-body">
+<h3 class="dropdown-item-title">
+John Pierce
+<span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+</h3>
+<p class="text-sm">I got your message bro</p>
+ <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+</div>
+</div>
+
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+
+<div class="media">
+<img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+<div class="media-body">
+<h3 class="dropdown-item-title">
+Nora Silvester
+<span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+</h3>
+<p class="text-sm">The subject goes here</p>
+<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+</div>
+</div>
+
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+</div>
+</li>
+
+<li class="nav-item dropdown">
+<a class="nav-link" data-toggle="dropdown" href="#">
+<i class="far fa-bell"></i>
+<span class="badge badge-warning navbar-badge">15</span>
+</a>
+<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+<span class="dropdown-item dropdown-header">15 Notifications</span>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+<i class="fas fa-envelope mr-2"></i> 4 new messages
+<span class="float-right text-muted text-sm">3 mins</span>
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+<i class="fas fa-users mr-2"></i> 8 friend requests
+<span class="float-right text-muted text-sm">12 hours</span>
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+<i class="fas fa-file mr-2"></i> 3 new reports
+<span class="float-right text-muted text-sm">2 days</span>
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+</div>
+</li>
+<li class="nav-item">
+<a class="nav-link" data-widget="fullscreen" href="#" role="button">
+<i class="fas fa-expand-arrows-alt"></i>
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+<i class="fas fa-th-large"></i>
+</a>
+</li>
+</ul>
+</nav>
+
+
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+<a href="index3.html" class="brand-link">
+<img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+<span class="brand-text font-weight-light">AdminLTE 3</span>
+</a>
+
+<div class="sidebar">
+
+<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+<div class="image">
+<img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+</div>
+<div class="info">
+<a href="#" class="d-block">Alexander Pierce</a>
+</div>
+</div>
+
+<div class="form-inline">
+<div class="input-group" data-widget="sidebar-search">
+<input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+<div class="input-group-append">
+<button class="btn btn-sidebar">
+<i class="fas fa-search fa-fw"></i>
+</button>
+</div>
+</div>
+</div>
+
+<nav class="mt-2">
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+<li class="nav-item menu-open">
+<a href="#" class="nav-link active">
+<i class="nav-icon fas fa-tachometer-alt"></i>
+<p>
+Dashboard
+<i class="right fas fa-angle-left"></i>
+</p>
+</a>
+<ul class="nav nav-treeview">
+<li class="nav-item">
+<a href="./index.html" class="nav-link">
+<i class="far fa-circle nav-icon"></i>
+<p>Dashboard v1</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="./index2.html" class="nav-link active">
+<i class="far fa-circle nav-icon"></i>
+<p>Dashboard v2</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="./index3.html" class="nav-link">
+<i class="far fa-circle nav-icon"></i>
+<p>Dashboard v3</p>
+</a>
+</li>
+
+
+</div>
+
+</aside>
+
+<div class="content-wrapper">
+
+<div class="content-header">
+<div class="container-fluid">
+<div class="row mb-2">
+<div class="col-sm-6">
+<h1 class="m-0">Dashboard v2</h1>
+</div>
+<div class="col-sm-6">
+<ol class="breadcrumb float-sm-right">
+<li class="breadcrumb-item"><a href="#">Home</a></li>
+<li class="breadcrumb-item active">Dashboard v2</li>
+</ol>
+</div>
+</div>
+</div>
+</div>
+
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+</section>
+
+</div>
+
+
+<aside class="control-sidebar control-sidebar-dark">
+
+</aside>
+
+
+<footer class="main-footer">
+<strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+All rights reserved.
+<div class="float-right d-none d-sm-inline-block">
+<b>Version</b> 3.2.0
+</div>
+</footer>
+</div>
+
+
+
+<script src="<?php echo base_url('assets')?>/plugins/jquery/jquery.min.js"></script>
+
+<script src="<?php echo base_url('assets')?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="<?php echo base_url('assets')?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+
+<script src="<?php echo base_url('assets')?>/dist/js/adminlte.js?v=3.2.0"></script>
+
+
+<script src="<?php echo base_url('assets')?>/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="<?php echo base_url('assets')?>/plugins/raphael/raphael.min.js"></script>
+<script src="<?php echo base_url('assets')?>/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="<?php echo base_url('assets')?>/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+
+<script src="<?php echo base_url('assets')?>/plugins/chart.js/Chart.min.js"></script>
+
+<script src="<?php echo base_url('assets')?>/dist/js/demo.js"></script>
+
+<script src="<?php echo base_url('assets')?>/dist/js/pages/dashboard2.js"></script>
+</body>
+</html>
